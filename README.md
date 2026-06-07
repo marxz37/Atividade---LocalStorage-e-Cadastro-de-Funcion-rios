b3e6cdb8e79d64257829fabca6eef36044838bd3
Marx Bento <152634196+marxz37@users.noreply.github.com>
2026-06-07 18:55:59 -0300

    feat: implementa sistema de cadastro e LocalStorage

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Atividade---LocalStorage-e-Cadastro-de-Funcion-rios
+# Cadastro de Funcionários
+
+Aplicação web desenvolvida para a prática de manipulação do DOM e uso do LocalStorage em JavaScript. O sistema permite o cadastro dinâmico de funcionários, exibição em formato de cards e persistência dos dados no navegador.
+
+A atividade será a base para evoluções futuras, como criação de uma tela de login ou controle de acesso.
+
+## 👨‍🎓 Aluno
+
+* **Nome:** Marx Bento Dumbá
+* **Matrícula:** 918513
+
+## Print da Aplicação
+
+![Print da Aplicação](/img/Print_Aplicacao.png)
+
+## Print do LocalStorage
+
+![Print da LocalStorage](/img/Print_LocalStorage.png)
---
+++ script.js
@@ -0,0 +1,59 @@
+const formFuncionario = document.getElementById('form-funcionario');
+const listaFuncionarios = document.getElementById('lista-funcionarios');
+const contadorElemento = document.getElementById('contador');
+
+let funcionarios = JSON.parse(localStorage.getItem('funcionariosBanco')) || [];
+
+function salvarNoLocalStorage() {
+    localStorage.setItem('funcionariosBanco', JSON.stringify(funcionarios));
+}
+
+function renderizarTela() {
+    listaFuncionarios.innerHTML = '';
+
+    contadorElemento.textContent = `Total: ${funcionarios.length}`;
+
+    funcionarios.forEach(funcionario => {
+        const divCard = document.createElement('div');
+        divCard.classList.add('card');
+
+        divCard.innerHTML = `
+            <h3>${funcionario.nome}</h3>
+            <p><strong>E-mail:</strong> ${funcionario.email}</p>
+            <p><strong>Cargo:</strong> ${funcionario.cargo}</p>
+            <p><strong>Departamento:</strong> ${funcionario.departamento}</p>
+        `;
+
+        listaFuncionarios.appendChild(divCard);
+    });
+}
+
+function cadastrarFuncionario(evento) {
+    evento.preventDefault();
+
+    const nomeInput = document.getElementById('nome').value;
+    const emailInput = document.getElementById('email').value;
+    const cargoInput = document.getElementById('cargo').value;
+    const departamentoInput = document.getElementById('departamento').value;
+    const senhaInput = document.getElementById('senha').value;
+
+    const novoFuncionario = {
+        id: Date.now(),
+        nome: nomeInput,
+        email: emailInput,
+        cargo: cargoInput,
+        departamento: departamentoInput,
+        senha: senhaInput
+    };
+
+    funcionarios.push(novoFuncionario);
+
+    salvarNoLocalStorage();
+    renderizarTela();
+
+    formFuncionario.reset();
+}
+
+formFuncionario.addEventListener('submit', cadastrarFuncionario);
+
+renderizarTela();
